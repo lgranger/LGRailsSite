5ed014d53aace142b1d8061deb655cd38446174b
Lauren Granger <lr.granger21@gmail.com>
2015-11-28 20:56:54 -0800

    categories and tags in form generated from database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,16 +3,20 @@ class PostsController < ApplicationController
   def new
     @login_allowed = true
     @post = Post.new
+    @categories = Category.all
+    @tags = Tag.all
     @action = "create"
   end
 
   def create
-    
+    Post.create()
   end
 
   def edit
     @login_allowed = true
     @post = Post.find(params[:id])
+    @categories = Category.all
+    @tags = Tag.all
     @action = "update"
   end
 
